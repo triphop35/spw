c74ff294a4ae94701f5c459be40fc64a8ba11050
triphop35 <triphop7768@gmail.com>
2015-03-12 18:44:09 +0700

    Update Enemy.java
    Edit

--- f2/spw/Enemy.java
+++ f2/spw/Enemy.java
@@ -8,7 +8,7 @@ public class Enemy extends Sprite{
 	public static final int Y_TO_FADE = 400;
 	public static final int Y_TO_DIE = 600;
 	
-	private int step = 12;
+	private int step = 13;
 	private boolean alive = true;
 	
 	public Enemy(int x, int y) {
@@ -24,7 +24,7 @@ public class Enemy extends Sprite{
 			g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 
 					(float)(Y_TO_DIE - y)/(Y_TO_DIE - Y_TO_FADE)));
 		}
-		g.setColor(Color.RED);
+		g.setColor(Color.BLUE);
 		g.fillRect(x, y, width, height);
 		
 	}
