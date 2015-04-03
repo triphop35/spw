ddfcd72e6ec651c3d8c28565f88d331dc91c8505
triphop35 <triphop7768@gmail.com>
2015-04-03 21:56:36 +0700

    Update GameEngine.java
    move up move down

--- f2/spw/GameEngine.java
+++ f2/spw/GameEngine.java
@@ -94,6 +94,12 @@ public class GameEngine implements KeyListener, GameReporter{
 		case KeyEvent.VK_D:
 			difficulty += 0.1;
 			break;
+		case KeyEvent.VK_UP:
++			v.moveup(-1);
++			break;
++		case KeyEvent.VK_DOWN:
++			v.moveup(1);
++			break;
 		}
 	}
 
