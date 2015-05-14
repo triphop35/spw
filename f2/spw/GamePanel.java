cc16951a0ab65261a89f03f0c58b2167ba1a5f7e
triphop35 <triphop7768@gmail.com>
2015-05-14 21:53:43 +0700

    ChangeBackground

--- f2/spw/GamePanel.java
+++ f2/spw/GamePanel.java
@@ -5,24 +5,29 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+import java.awt.Toolkit;
+import java.awt.Image;
 
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel {
 	
-	private BufferedImage bi;	
+	private BufferedImage bi;
+	private Image imgBackground;	
 	Graphics2D big;
 	ArrayList<Sprite> sprites = new ArrayList<Sprite>();
 
 	public GamePanel() {
+		imgBackground = Toolkit.getDefaultToolkit().getImage("bg.jpg");
 		bi = new BufferedImage(400, 600, BufferedImage.TYPE_INT_ARGB);
 		big = (Graphics2D) bi.getGraphics();
-		big.setBackground(Color.BLACK);
+		//big.setBackground(Color.BLACK);
+		big.drawImage(imgBackground, 0, 0, 400, 600,null);
 	}
 
 	public void updateGameUI(GameReporter reporter){
-		big.clearRect(0, 0, 400, 600);
-		
+		//big.clearRect(0, 0, 400, 600);
+		big.drawImage(imgBackground, 0, 0, 400, 600,null);
 		big.setColor(Color.WHITE);		
 		big.drawString(String.format("%08d", reporter.getScore()), 300, 20);
 		for(Sprite s : sprites){
